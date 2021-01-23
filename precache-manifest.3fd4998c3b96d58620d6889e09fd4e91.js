0b6f72d602f502aa219b1353c8132fb707f100cd
Phan Xuân Vũ <pxvu.17IT2@sict.udn.vn>
2021-01-23 11:32:00 +0700

    Updates

---
+++ precache-manifest.3fd4998c3b96d58620d6889e09fd4e91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8c8d38cbef65daecffb393e6f7b1d7",
+    "url": "/FashionWeb/index.html"
+  },
+  {
+    "revision": "a0a51a58b1c573a62eb6",
+    "url": "/FashionWeb/static/css/2.ef3a71c5.chunk.css"
+  },
+  {
+    "revision": "174479b4b42510262fdc",
+    "url": "/FashionWeb/static/css/main.da14c628.chunk.css"
+  },
+  {
+    "revision": "a0a51a58b1c573a62eb6",
+    "url": "/FashionWeb/static/js/2.7a2e59f1.chunk.js"
+  },
+  {
+    "revision": "c4c37adef2c97909af604880d0f75d7f",
+    "url": "/FashionWeb/static/js/2.7a2e59f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174479b4b42510262fdc",
+    "url": "/FashionWeb/static/js/main.970d06c0.chunk.js"
+  },
+  {
+    "revision": "e679c6f8ab600f1a401d",
+    "url": "/FashionWeb/static/js/runtime-main.45dd2928.js"
+  }
+]);
